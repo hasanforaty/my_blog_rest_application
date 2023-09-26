f546f3e0c81e9ba9cbd31aff084cf6fc5fc9f0bd
hasan foraty <hasanforaty@gmail.com>
2023-09-26 09:53:32 +0330

    git init

---
+++ src/test/java/com/hasan/foraty/myblogapplication/BlogRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasan.foraty.myblogapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogRestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

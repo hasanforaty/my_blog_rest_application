260d8ecce310b7a89028adb16eb882983218e263
hasan foraty <hasanforaty@gmail.com>
2023-10-07 08:46:38 +0330

    category entity

---
+++ src/main/java/com/hasan/foraty/myblogapplication/entity/Category.java
@@ -0,0 +1,37 @@
+package com.hasan.foraty.myblogapplication.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "categories")
+public class Category {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(name = "name")
+  private String name;
+  @Column(name = "description")
+  private String description;
+
+  @OneToMany(mappedBy = "category",cascade = CascadeType.ALL,orphanRemoval = true)
+  List<Post> posts;
+
+}
--- src/main/java/com/hasan/foraty/myblogapplication/entity/Post.java
+++ src/main/java/com/hasan/foraty/myblogapplication/entity/Post.java
@@ -3,9 +3,12 @@ package com.hasan.foraty.myblogapplication.entity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
@@ -43,6 +46,10 @@ public class Post {
 
   @OneToMany(mappedBy = "post",cascade = {CascadeType.ALL})
   List<Comment> comments;
+
+  @ManyToOne(fetch = FetchType.LAZY,cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+  @JoinColumn(name = "category_id")
+  private Category category;
 
   public void addComment(Comment comment){
     if(comments==null){
